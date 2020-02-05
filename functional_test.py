6f87b1f280243a43a8bdfbbca62d48779fdf8125
Bartosz <bartosz.lewosz@gmail.com>
2020-02-05 00:34:13 +0000

    Check second entry to the list item

--- functional_test.py
+++ functional_test.py
@@ -31,29 +31,28 @@ class NewVisitorTest(unittest.TestCase):
         )
 # She types "Buy peacock feathers" into a text box
 #  (Edith's hobby is tying fly-fish lures)
-        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Use peacock feathers to make a fly')
 
 # When she hits enter, the page updates, and now the page lists
 # "1: buy pracock feathers" as an item in a to-do list
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
 # There is still text box inviting her to add another item.
 # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        
 # The page updates again, and now shows both items on her list
 
 # Edith wonder wheter the site will remember her list. The she sees
 # that site has generated a unique URL for her -- there is some explanatory text to that effect
 
+        self.fail('Finish the test!')
 # She visits URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep
