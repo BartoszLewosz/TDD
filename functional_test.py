7f73cbdecb23e61ca8f5a7613d690c54019d5d41
Bartosz <bartosz.lewosz@gmail.com>
2020-02-07 01:50:02 +0000

    Small bug fix

--- functional_test.py
+++ functional_test.py
@@ -36,7 +36,7 @@ class NewVisitorTest(unittest.TestCase):
         )
 # She types "Buy peacock feathers" into a text box
 #  (Edith's hobby is tying fly-fish lures)
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('Buy peacock feathers')
 
 # When she hits enter, the page updates, and now the page lists
 # "1: buy pracock feathers" as an item in a to-do list
@@ -45,12 +45,17 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(2)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
 # There is still text box inviting her to add another item.
 # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
 # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 # Edith wonder wheter the site will remember her list. The she sees
 # that site has generated a unique URL for her -- there is some explanatory text to that effect
