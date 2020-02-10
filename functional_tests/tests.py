6c530e073efd0c042bd6d045c87083e9230fb4a3
Bartosz <bartosz.lewosz@gmail.com>
2020-02-10 11:26:31 +0000

    Begin to test styles and layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -46,6 +46,24 @@ class NewVisitorTest(LiveServerTestCase):
             'Enter a to-do item'
         )
 
+    def test_layout_and_styling(self):
+        # She goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices that input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
+        )
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
+        )
+
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
